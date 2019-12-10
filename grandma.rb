6c37a87a2c6856db5561c28d278391e94dd3f4ce
Joshua Rodriguez <josh030596@gmail.com>
2019-12-10 17:47:59 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 def speak_to_grandma(speak)
   speak = "Hi Nana, how are you?" 
-  if speak == "Hi Nana, how are you?".upcase
+  if speak.upcase
       "HUH?! SPEAK UP, SONNY!"
   elsif speak == "return"
       "NO, NOT SINCE 1938"
